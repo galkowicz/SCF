d62b45afeb5567145f00dc59fd5df9523e04da0a
galkow <orian@mail.sapir.ac.il>
2016-01-17 01:37:28 +0200

    added search

--- app/app.js
+++ app/app.js
@@ -8,21 +8,28 @@ app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
         client_id: 'd652006c469530a4a7d6184b18e16c81'
     });
 
-    $http({
-        method: 'GET',
-        url: 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q=sprout%20and%20the%20bean%7Cparks&limit=10'
-    }).then(function successCallback(response) {
-        var songs = [];
-        response.data.forEach(function (element, index, array) {
-            songs.push(element);
-        });
-        $scope.songs = songs;
-        console.log(songs);
 
-    }, function errorCallback(response) {
-        console.log("err");
-        console.log(response);
-    });
+
+    $scope.showResults = function (search){
+        console.log("searching...");
+        var searchString = 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q='+search+'%7Cparks&limit=10';
+        $http({
+            method: 'GET',
+            url: searchString
+        }).then(function successCallback(response) {
+            var songs = [];
+            response.data.forEach(function (element, index, array) {
+                songs.push(element);
+            });
+            $scope.songs = songs;
+            console.log(songs);
+
+        }, function errorCallback(response) {
+            console.log("err");
+            console.log(response);
+        });
+
+    }
     var currentSongIndex = 0;
     $scope.showImg = function (index) {
         $scope.img = $scope.songs[index].artwork_url;
@@ -38,13 +45,25 @@ app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
         SC.oEmbed(track_url, {auto_play: true}).then(function (oEmbed) {
             console.log('oEmbed response: ', oEmbed.html);
             angular.element(document.getElementById('widget')).append(oEmbed.html);
-            
-            //$scope.widget = oEmbed.html;
+
 
         });
     }
 
 
 }]);
+
+
+app.controller('searchController', function($scope) {
+
+        $scope.seacrh = "Seacrh";
+        $scope.data = [];
+        $scope.submitForm = function(search) {
+            $scope.data.push(search);
+            console.log("searching... and pushing");
+         };
+    });
+
+
 
 // http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q=radiohead%7Cparks&limit=2
---
+++ app/directives/directives.js
@@ -0,0 +1,3 @@
+/**
+ * Created by orian on 16/01/2016.
+ */
