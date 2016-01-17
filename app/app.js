14715d7d7bc8c79319145f1f9908892be99ab157
galkow <orian@mail.sapir.ac.il>
2016-01-17 19:37:48 +0200

    history and button

--- app/app.js
+++ app/app.js
@@ -9,54 +9,59 @@ app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
     });
 
 
+    var page_size = 6;
 
     $scope.showResults = function (search){
         console.log("searching...");
-        var searchString = 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q='+search+'%7Cparks&limit=10';
+        var searchString = 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q='+search+'';
         $http({
             method: 'GET',
-            url: searchString
+            url: searchString,
+            params: {limit: page_size}//, linked_partitioning: 1 }
+
         }).then(function successCallback(response) {
             var songs = [];
             response.data.forEach(function (element, index, array) {
                 songs.push(element);
             });
+            console.log("the response---------------------: "+ response.data);
             $scope.songs = songs;
-            console.log(songs);
+            //console.log(songs);
 
         }, function errorCallback(response) {
             console.log("err");
             console.log(response);
         });
 
+
     }
     var currentSongIndex = 0;
     $scope.showImg = function (index) {
         $scope.img = $scope.songs[index].artwork_url;
         currentSongIndex = index;
-        console.log(index);
+        //console.log(index);
     }
 
     $scope.showWidget = function () {
         var track_url = $scope.songs[currentSongIndex].permalink_url;
-        console.log(track_url);
-        //console.log($scope.songs[currentSongIndex].permalink_url);
-        //console.log($scope.songs);
         SC.oEmbed(track_url, {auto_play: true}).then(function (oEmbed) {
             console.log('oEmbed response: ', oEmbed.html);
             angular.element(document.getElementById('widget')).append(oEmbed.html);
-
+           //angular.element(document.getElementById('widger')).replace(oEmbed.html);
+            //TODO make the widget replace the old one and not append
 
         });
     }
+    $scope.nextResults = function () {
 
+    }
 
 }]);
 
 
 app.controller('searchController', function($scope) {
 
-        $scope.seacrh = "Seacrh";
+        $scope.search = "Search";
         $scope.data = [];
         $scope.submitForm = function(search) {
             $scope.data.push(search);
@@ -65,5 +70,13 @@ app.controller('searchController', function($scope) {
     });
 
 
+app.controller('saveHistory', function($scope) {
 
-// http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q=radiohead%7Cparks&limit=2
+    $scope.itmes = [];
+    $scope.addToHistory = function(search) {
+        $scope.itmes.push(search);
+        console.log("adding to history");
+        console.log("history data: "+$scope.itmes);
+    };
+});
+
