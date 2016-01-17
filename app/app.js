66b2d029e1982b6ca146b010d4c3b1a53874048a
galkow <orian@mail.sapir.ac.il>
2016-01-18 00:17:41 +0200

    final?

--- app/app.js
+++ app/app.js
@@ -1,33 +1,48 @@
 'use strict';
 
-var app = angular.module('myApp', ['ngSanitize']);
+var app = angular.module('myApp', ['ngStorage']);
 
 
-app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
+app.controller('SoundCloud', ['$http', '$scope','$localStorage', function ($http, $scope, $localStorage) {
     SC.initialize({
         client_id: 'd652006c469530a4a7d6184b18e16c81'
     });
-
-
-    var page_size = 6;
-
+    //$scope.$storage = $localStorage.$default({
+    //    x: 42
+    //});
+    //$localStorage.sss = 'sa';
+    //console.log($localStorage);
+    $scope.$storage = $localStorage.$default({
+        recents: []
+    });
+    $scope.tile = true;
     $scope.showResults = function (search){
+        $scope.$storage.recents.unshift(search);
+        $scope.startIndex = 0;
+        $scope.pageNum=0;
+
+        //currentSongIndex=0;
+
+        if ($scope.$storage.recents.length > 5) {
+            $scope.$storage.recents.splice(5,1);
+        }
+        console.log($scope.$storage);
         console.log("searching...");
-        var searchString = 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q='+search+'';
+        var searchString = 'http://api.soundcloud.com/tracks.json?client_id=d652006c469530a4a7d6184b18e16c81&q='+search+'&limit=50';
         $http({
             method: 'GET',
             url: searchString,
-            params: {limit: page_size}//, linked_partitioning: 1 }
+            params: {}//, linked_partitioning: 1 }
 
         }).then(function successCallback(response) {
             var songs = [];
             response.data.forEach(function (element, index, array) {
                 songs.push(element);
             });
-            console.log("the response---------------------: "+ response.data);
+            //console.log(response);
             $scope.songs = songs;
-            //console.log(songs);
-
+            console.log(songs);
+            $scope.arrSize = songs.length;
         }, function errorCallback(response) {
             console.log("err");
             console.log(response);
@@ -39,20 +54,33 @@ app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
     $scope.showImg = function (index) {
         $scope.img = $scope.songs[index].artwork_url;
         currentSongIndex = index;
+        //angular.element(document.getElementById('widget')).removeData();
+        //$scope.playing = false;
         //console.log(index);
     }
 
     $scope.showWidget = function () {
-        var track_url = $scope.songs[currentSongIndex].permalink_url;
-        SC.oEmbed(track_url, {auto_play: true}).then(function (oEmbed) {
-            console.log('oEmbed response: ', oEmbed.html);
-            angular.element(document.getElementById('widget')).append(oEmbed.html);
-           //angular.element(document.getElementById('widger')).replace(oEmbed.html);
-            //TODO make the widget replace the old one and not append
+        //if (!$scope.playing) {
+            var track_url = $scope.songs[currentSongIndex].permalink_url;
+            //$scope.playing = true;
+            SC.oEmbed(track_url, {auto_play: true,maxheight: 100, maxwidth: 300}).then(function (oEmbed) {
+                angular.element(document.getElementById('widget')).html(oEmbed.html);
 
-        });
+            });
+        //}
     }
+
+     $scope.size = 6;
+    $scope.startIndex = 0;
     $scope.nextResults = function () {
+        console.log($scope.startIndex);
+        $scope.startIndex += $scope.size;
+
+        if ($scope.startIndex >= ($scope.arrSize) ) {
+            $scope.startIndex = 0;
+            $scope.pageNum=0;
+        }
+        $scope.pageNum+= 1;
 
     }
 
@@ -62,21 +90,36 @@ app.controller('SoundCloud', ['$http', '$scope', function ($http, $scope) {
 app.controller('searchController', function($scope) {
 
         $scope.search = "Search";
-        $scope.data = [];
         $scope.submitForm = function(search) {
             $scope.data.push(search);
-            console.log("searching... and pushing");
+            //console.log("searching... and pushing");
          };
     });
 
 
-app.controller('saveHistory', function($scope) {
+//app.controller('saveHistory', function($scope, $localStorage){
+//
+//    $scope.addToHistory = function(){
+//        $localStorage.prevPageData = $scope.search;
+//    };
+//
+//});
 
-    $scope.itmes = [];
-    $scope.addToHistory = function(search) {
-        $scope.itmes.push(search);
-        console.log("adding to history");
-        console.log("history data: "+$scope.itmes);
-    };
+app.filter('filter6', function () {
+    return function (input , startFrom , size) {
+
+
+        if (!input)  return;
+        return input.slice(startFrom, startFrom+size);
+
+    }
 });
 
+
+//angular.module('app', [
+//    'ngStorage'
+//]).controller('Ctrl', function(
+//    $scope,
+//    $localStorage,
+//    $sessionStorage
+//){});
